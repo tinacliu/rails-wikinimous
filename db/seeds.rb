70c3241461e0a7ffd3716e348520b2383ca1af17
Tina Liu <tina.chang.liu@gmail.com>
2019-05-21 17:03:31 +0900

    added code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+i = 1
+10.times do
+  article = Article.new(
+    title: Faker::GreekPhilosophers.name,
+    content: Faker::GreekPhilosophers.quote,
+    ranking: i
+  )
+  i += 1
+  article.save
+end
